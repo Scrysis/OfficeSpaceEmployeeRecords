449404013e9321887633901e0cace6794a19fc75
Scrysis <Scrysis@users.noreply.github.com>
2024-01-04 15:13:02 -0700

    working database schema setup

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'password',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
